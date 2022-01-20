6c684a546c7b85e505d306a70a5ab93fb8198d09
Anna Khuseinova <annakhuseinova@yandex.ru>
2022-01-21 02:12:10 +0300

    Initial commit

---
+++ src/main/java/com/annakhuseinova/springwebfluxcaching/SpringWebfluxCachingApplication.java
@@ -0,0 +1,13 @@
+package com.annakhuseinova.springwebfluxcaching;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebfluxCachingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWebfluxCachingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/annakhuseinova/springwebfluxcaching/SpringWebfluxCachingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.annakhuseinova.springwebfluxcaching;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebfluxCachingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
